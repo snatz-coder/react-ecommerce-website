40b970b795ada8131308e742997038be4577375b
sujane <snazlopez@gmail.com>
2021-05-06 04:04:51 +0530

    created node server

---
+++ backend/data.js
@@ -0,0 +1,38 @@
+export default  {
+    products: [{
+        _id:1,
+        name: "Slim Shirt",
+        category: 'shirts',
+        image: '/images/d1.jpg',
+        brand: "Nike",
+        price:60,
+        rating:4.5,
+        numReviews:10
+
+    },
+    {
+        _id:2,
+        name: "Fit Shirt",
+        category: 'shirts',
+        image: '/images/d1.jpg',
+        brand: "Nike",
+        price:70,
+        rating:4.2,
+        numReviews:10
+
+    },
+    {
+        _id:3,
+        name: "BodyFit pants",
+        category: 'pants',
+        image: '/images/d1.jpg',
+        brand: "Nike",
+        price:50,
+        rating:4.3,
+        numReviews:10
+
+    }
+
+    ]
+}
+
